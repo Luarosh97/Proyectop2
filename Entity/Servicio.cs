76bbfc32d701373a312a3fc741899393880e2fa9
Rochety <Rochety@DESKTOP-AVMJ5D9>
2020-05-31 12:27:55 +0200

    Agregar archivos de proyecto.

---
+++ Entity/Servicio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Servicio
+    {
+       public  string CodigodeServicio {get;set;}
+       public string NombreServicio {get; set;}
+    
+       public int PrecioServicio {get;set;}
+       
+  
+     public Servicio()
+        {
+
+        }
+
+        public Servicio(string codigoservicio, string nombreservicio, decimal precioServicio)
+        { 
+            CodigodeServicio = codigoservicio;
+            NombreServicio = nombreservicio;
+            
+        
+            
+        }
+
+        
+    }
+}
